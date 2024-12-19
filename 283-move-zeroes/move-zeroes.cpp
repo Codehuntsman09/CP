7f126b96ba9354798f53749aebac1143efacfa06
codehuntsman <91817963+code-huntsman@users.noreply.github.com>
2024-12-19 09:30:12 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (6.49%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -3,19 +3,17 @@ public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
         int j = 0;
-        for(int i=0; i<n; ++i) {
-            if(nums[i] == 0) {
-                j = i;
+        while(j < n) {
+            if(nums[j] == 0) {
                 break;
             }
+            j++;
         }
-        if(nums[j] == 0) {
             for(int i=j+1; i<n; ++i) {
                 if(nums[i] != 0) {
                     swap(nums[i], nums[j]);
                     j++;
                 }
             }
-        }
     }
 };
